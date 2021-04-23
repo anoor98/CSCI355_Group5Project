a50e11c59c3db98d249e596f6146367b25aa72d8
anoor98 <aliemanoor@gmail.com>
2021-04-22 21:24:59 -0400

    New DB edits

--- Group Project/Group/Demo_Database.sql
+++ Group Project/Group/Demo_Database.sql
@@ -8,3 +8,27 @@ CREATE TABLE LOGINS (
     MIDDLE_NAME varchar(40),
     FORM_STATUS varchar(40)
 );
+
+/*
+3 tables for 3 different forms
+all connect with a foriegn key that connects to the student
+each table would have a row corresponding to the information given
+*/
+CREATE TABLE PAGE1 (	
+	STUDENT_KEY INT(100) NOT NULL FOREIGN KEY,
+    NAME_ varchar(40) NOT NULL PRIMARY KEY, 
+	Student_ID INT(8) NOT NULL,
+    FROM_DATE DATE Not Null,
+    TO_DATE DATE Not NULL,
+    UNDER_18 Boolean,
+    UNDER_23 Boolean,
+    /*
+    Freshman, transfer, seek, etc
+    */
+    REQUESTED_BY DATE not null,
+    _DATE_ DATE not null
+);
+
+
+Select *
+From LOGINS
