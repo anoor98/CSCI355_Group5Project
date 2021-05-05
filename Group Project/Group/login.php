bff870eb8521c432d155c5659731c533e04dc0ee
Kaan Dogrusoz <kaandogrusoz@Kaans-MacBook-Pro.local>
2021-05-05 15:37:56 -0400

    update php

--- Group Project/Group/login.php
+++ Group Project/Group/login.php
@@ -1,8 +1,7 @@
 <?php
     $username = $_POST["username"];
     $password = $_POST["password"];
-        echo $password; 
-        echo $username;
+
     if (!empty($username) && !empty($password)){
         $host = "mars.cs.qc.cuny.edu";
         $dbUsername = "doen5420"; 
@@ -15,19 +14,18 @@
         if (mysqli_connect_error()){
             die('Connect Error('. mysqli_connect_errno(). ')'. mysqli_connect_error());
         } 
-	else {
-            //$result = mysql_query("SELECT USERNAME, PASS_WORD FROM LOGINS where USERNAME =  '$username' and PASS_WORD = '$password'")
-              //          or die("Failed to query db ".mysql_error());
-        $sql = "SELECT USERNAME, PASS_WORD FROM LOGINS where USERNAME =  '$username' and PASS_WORD = '$password'";
-        $result	= $mysqli->query($sql);
-        while($row = $results->fetch_assoc());
-            if ($row['USERNAME'] == $username && $row['PASS_WORD'] == $password){
-                echo "Login Success";
+	    else {
+            echo "connected to database";
+            $sql = "SELECT USERNAME, PASS_WORD FROM LOGINS where USERNAME =  '$username' and PASS_WORD = '$password'";
+            $result	= $mysqli->query($sql);
+            while($row = $results->fetch_assoc());
+                if ($row['USERNAME'] == $username && $row['PASS_WORD'] == $password){
+                    echo "Login Success";
 
-            }else{
-                echo "Failed Login";
-            }
-	}
+                }else{
+                    echo "Failed Login";
+                }
+	    }
     }else {
 	echo " All fields are required";
         die();
